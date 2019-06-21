d8ff9acc9e987a790d08dd9344ded07c20ebf454
balloonhotair <51388216+balloonhotair@users.noreply.github.com>
2019-06-21 15:30:21 -0700

    Alpha pass at translations

---
+++ highcharts/translations.js
@@ -0,0 +1,9 @@
+var translations = { 
+'Temperature':'Temperature',
+'Feelslike':'Feels',
+'Humidity':'Humidity',
+'Dewpoint':'Dewpoint',
+'Trend':'trend ',
+'Heatindex':'Heat Index',
+'Windchill':'Windchill '};
+
