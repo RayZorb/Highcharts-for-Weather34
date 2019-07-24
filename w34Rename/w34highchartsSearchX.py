8b6b241dfabda95486da41ee4a2cf81d521124d8
balloonhotair <51388216+balloonhotair@users.noreply.github.com>
2019-07-24 00:31:52 -0700

    Combined highcharts files

--- w34Rename/w34highchartsSearchX.py
+++ w34Rename/w34highchartsSearchX.py
@@ -34,9 +34,11 @@ import json
 import syslog
 import time
 import weewx
+import math
+import weeutil.weeutil
 
+from weewx.units import getStandardUnitType, ValueTuple
 from datetime import date
-from user.highcharts import getDaySummaryVectors
 from weewx.cheetahgenerator import SearchList
 from weewx.units import ValueTuple, getStandardUnitType, convert, _getUnitGroup
 from weeutil.weeutil import TimeSpan, genMonthSpans, startOfInterval, option_as_list, startOfDay
@@ -91,7 +93,98 @@ def get_ago(dt, d_years=0, d_months=0):
     # Calculate and return date object
     _eom = calendar.monthrange(_y + _a, _m + 1)[1]
     return date(_y + _a, _m + 1, _d if _d <= _eom else _eom)
+def getDaySummaryVectors(db_manager, sql_type, timespan, agg_list='max'):
+    """ Return a vector of specified stats from weewx daily summaries.
 
+        Parameters:
+          db_manager: A database manager object for the weewx archive.
+
+          sql_type:   A statistical type, such as 'outTemp' 'barometer' etc.
+
+          startstamp: The start time of the vector required.
+
+          stopstamp:  The stop time of the vector required.
+
+          agg_list:   A list of the aggregates required eg ['max', 'min'].
+                      Member elements can be any of 'min', 'max', 'mintime',
+                      'maxtime', 'gustdir', 'sum', 'count', 'avg', 'rms',
+                      'vecavg' or 'vecdir'.
+       """
+
+    # Get our interpolation dictionary for the query
+    interDict = {'start'        : weeutil.weeutil.startOfDay(timespan.start),
+                 'stop'         : timespan.stop,
+                 'table_name'   : 'archive_day_%s' % sql_type}
+    # Setup up a list of lists for our vectors
+    _vec = [list() for x in range(len(agg_list))]
+    # Initialise each list in the list of lists
+    for agg in agg_list:
+        _vec[agg_list.index(agg)] = list()
+    # Setup up our time vector list
+    _time_vec = list()
+    # Initialise a dictionary for our results
+    _return = {}
+    # Get the unit system in use
+    _row = db_manager.getSql("SELECT usUnits FROM %s LIMIT 1;" % db_manager.table_name)
+    std_unit_system = _row[0] if _row is not None else None
+    # Get a cursor object for our query
+    _cursor=db_manager.connection.cursor()
+    try:
+        # Put together our SQL query string
+        sql_str = "SELECT * FROM %(table_name)s  WHERE dateTime >= %(start)s AND dateTime < %(stop)s" % interDict
+        # Loop through each record our query returns
+        for _rec in _cursor.execute(sql_str):
+            # Loop through each aggregate we have been asked for
+            for agg in agg_list:
+                # Calculate the aggregate
+                if agg == 'min':
+                    _result = _rec[1]
+                elif agg == 'max':
+                    _result = _rec[3]
+                elif agg == 'sum':
+                    _result = _rec[5]
+                elif agg == 'gustdir':
+                    _result = _rec[7]
+                elif agg == 'mintime':
+                    _result = int(_rec[2]) if _rec[2] else None
+                elif agg == 'maxtime':
+                    _result = int(_rec[4]) if _rec[4] else None
+                elif agg == 'count':
+                    _result = int(_rec[6]) if _rec[6] else None
+                elif agg == 'avg' :
+                    _result = _rec[5]/_rec[6] if (_rec[5] and _rec[6]) else None
+                elif agg == 'rms' :
+                    _result =  math.sqrt(_rec[10]/_rec[11]) if (_rec[10] and _rec[11]) else None
+                elif agg == 'vecavg' :
+                    _result = math.sqrt((_rec[8]**2 + _rec[9]**2) / _rec[6]**2) if (_rec[6] and _rec[8] and _rec[9]) else None
+                elif agg == 'vecdir' :
+                    if _rec[8] == 0.0 and _rec[9] == 0.0:
+                        _result = None
+                    elif _rec[8] and _rec[9]:
+                        deg = 90.0 - math.degrees(math.atan2(_rec[9], _rec[8]))
+                        _result = deg if deg >= 0.0 else deg + 360.0
+                    else:
+                        _result = None
+                # If we have not found it then return None
+                else:
+                    _result = None
+                # Add the aggregate to our vector
+                _vec[agg_list.index(agg)].append(_result)
+            # Add the time to our time vector
+            _time_vec.append(_rec[0])
+    finally:
+        # Close our cursor
+        _cursor.close()
+    # Get unit type and group for time
+    (_time_type, _time_group) = weewx.units.getStandardUnitType(std_unit_system, 'dateTime')
+    # Loop through each aggregate we were asked for getting unit and group and producing a ValueTuple
+    # and adding to our result dictionary
+    for agg in agg_list:
+        (t,g) = weewx.units.getStandardUnitType(std_unit_system, sql_type, agg)
+        _return[agg]=ValueTuple(_vec[agg_list.index(agg)], t, g)
+    # Return our time vector and dictionary of aggregate vectors
+    return (ValueTuple(_time_vec, _time_type, _time_group), _return)
+    
 class w34highchartsMinRanges(SearchList):
     """SearchList to return y-axis minimum range values for each plot."""
 
