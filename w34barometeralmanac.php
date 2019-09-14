b9f455d8dc6cca6bdc52aa4fe019abdc1d522cf5
Ian Millard <steepleian@gmail.com>
2019-09-14 07:14:36 +0100

    Update w34barometeralmanac.php

--- w34barometeralmanac.php
+++ w34barometeralmanac.php
@@ -210,7 +210,7 @@ align-items:center;justify-content:center;margin-bottom:10px;top:0}
   <main class="grid1">
   <articlegraph> 
   <div class=actualt><?php echo date('Y')?> Barometer Chart</div>  
-  <iframe  src="highcharts/dark-charts.html?chart='barsmallplot'&span='yearly'&temp='<?php echo $weather['temp_units'];?>'&pressure='<?php echo $weather['barometer_units'];?>'&wind='<?php echo $weather['wind_units'];?>'&rain='<?php echo $weather['rain_units']?>" frameborder="0" scrolling="no" width="100%" height="205px"></iframe>
+  <iframe  src="w34highcharts/dark-charts.html?chart='barsmallplot'&span='yearly'&temp='<?php echo $weather['temp_units'];?>'&pressure='<?php echo $weather['barometer_units'];?>'&wind='<?php echo $weather['wind_units'];?>'&rain='<?php echo $weather['rain_units']?>" frameborder="0" scrolling="no" width="100%" height="205px"></iframe>
    
   </articlegraph> 
   
