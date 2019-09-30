1284f877f350cd2b58273365aa6c609cfa4834b3
Ian Millard <steepleian@gmail.com>
2019-09-30 19:33:18 +0200

    Update plots.js

--- w34highcharts/scripts/plots.js
+++ w34highcharts/scripts/plots.js
@@ -417,8 +417,8 @@ function addWeekOptions(obj, span, seriesData, units, plot_type) {
         text: '24h'
     }, {
         type: 'hour',
-        count: 36,
-        text: '36h'
+        count: 35,
+        text: '35h'
     }, {
         type: 'all',
         text: compare_dates  || day_plots ? '72h' : '7d'
@@ -449,8 +449,8 @@ function addYearOptions(obj) {
         text: '1m'
     }, {
         type: 'month',
-        count: 6,
-        text: '6m'
+        count: 5,
+        text: '5m'
     }, {
         type: 'all',
         text: '1y'
